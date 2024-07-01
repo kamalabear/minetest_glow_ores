5250481ff8ddc738ebed876c96ed7b5e1b9f27c6
kamalabear <kamala.ferris@gmail.com>
2024-07-01 21:46:20 +0200

    feature: actually adding the light_source calculation from scarcity of the ore

--- init.lua
+++ init.lua
@@ -1,25 +1,71 @@
 local S = minetest.get_translator("glow_ores")
 
+-- function printme(i)
+--     if not i then
+--         return "nil"
+--     else
+--         local val = "{\n"
+--         for k, v in pairs(i) do
+--             if type(v) == "table" then
+--                 val = val..k.." = {"..printme(v).."}, "
+--             else
+--                 val = val..k.." = "..tostring(v)..", "
+--             end
+--         end
+--         val = val.."}"
+--         return val
+--     end
+-- end
+
+-- Get the default light_source level from the configuration
 default_glow_level = tonumber(minetest.settings:get("default_glow_level"))
 if default_glow_level == nil then
     default_glow_level = 4
 end
 
+-- The maximum light_source value supported
+local MAX = minetest.LIGHT_MAX
+
+-- Reduce the number to something between 4 and LIGHT_MAX (for use as a light_source value)
+function reduce(num)
+    num = ( math.sqrt (num) / 1000 * MAX * (MAX - 4 + 1) + 4 ) / 2
+    if num > MAX then
+        num = MAX
+    elseif num < 4 then
+        num = 4
+    end
+    return num
+end
+
+-- Determine the light_source level for the passed ore.
+function get_glow_level(ore)
+    minetest.log("info", "[glow_ores]Getting glow level for "..(ore.ore or "nil"))
+    if ore == nil then
+        minetest.log("debug", "[glow_ores]In get_glow_level... ore is nil")
+        return default_glow_level
+    elseif ore.clust_scarcity then
+        minetest.log("debug", "[glow_ores]In get_glow_level... using clust_scarcity of "..ore.clust_scarcity)
+        return reduce(ore.clust_scarcity)
+    elseif ore.clust_size then
+        minetest.log("debug", "[glow_ores]In get_glow_level... using clust_size of "..ore.clust_size)
+        return reduce(ore.clust_size)
+    else
+        minetest.log("debug", "[glow_ores]In get_glow_level... using default glow level")
+        return default_glow_level
+    end 
+end
+
 minetest.log("[glow_ores]default_glow_level = "..default_glow_level)
 
-for name, reg in pairs(minetest.registered_ores) do
-	if reg.ore_type == "scatter" then
-        minetest.log("[glow_ores]Found ore "..reg.ore)
-        ore = minetest.registered_nodes[reg.ore]
-        itemname = ore.name
-        itemname = string.gsub(itemname, ":", "_")
-        is_setting_key = "is_glow_"..itemname
-        is_setting = minetest.settings:get_bool(is_setting_key)
-        level_setting_key = itemname.."_glow_level"
-        level_setting = tonumber(minetest.settings:get(level_setting_key))
-        if is_setting == nil and default_glow_level and default_glow_level > 0 then
-            minetest.log("[glow_ores]Using the default glow level")
-            ore.light_source = default_glow_level
+-- Rainbow ore doesn't register as an ore until after other mods are loaded (so that it can respect its spawn
+-- within configuration).  Therefore, it won't be found in this loop unless we wait too.
+minetest.register_on_mods_loaded(function()
+    for name, reg in pairs(minetest.registered_ores) do
+        if reg.ore_type == "scatter" then
+            minetest.log("[glow_ores]Found ore "..reg.ore)
+            ore = minetest.registered_nodes[reg.ore]
+            itemname = ore.name
+            itemname = string.gsub(itemname, ":", "_")
             description = ore.description
             tiles = ore.tiles
             groups = ore.groups
@@ -29,13 +75,25 @@ for name, reg in pairs(minetest.registered_ores) do
             local use_texture_alpha = ore.use_texture_alpha
             local drawtype = ore.drawtype
             local sunlight_propagates = ore.sunlight_propagates
+            is_setting_key = "is_glow_"..itemname
+            is_setting = minetest.settings:get_bool(is_setting_key)
+            is_custom = minetest.settings:get_bool("is_custom_"..itemname)
+            if is_setting ~= nil and is_custom then
+                level_setting_key = itemname.."_glow_level"
+                level_setting = tonumber(minetest.settings:get(level_setting_key))
+            else
+                level_setting = get_glow_level(reg)
+            end
+
+            minetest.log("[glow_ores]Setting light_source of "..ore.name.." to "..level_setting)
+
             minetest.register_node(":"..ore.name, {
                 description = description,
                 tiles = tiles,
                 groups = groups,
                 drop = drop,
                 is_ground_content = is_ground_content,
-                light_source = default_glow_level,
+                light_source = level_setting,
                 paramtype = paramtype,
                 use_texture_alpha = use_texture_alpha,
                 drawtype = drawtype,
@@ -43,287 +101,4 @@ for name, reg in pairs(minetest.registered_ores) do
             })
         end
     end
-end
-
-local is_glow_coal = minetest.settings:get_bool("is_glow_coal")
-local coal_glow_level = tonumber(minetest.settings:get("coal_glow_level"))
-local ore = minetest.registered_nodes["default:stone_with_coal"]
-if ore and is_glow_coal then
-    --Redefine coal ore
-    minetest.log("[glow_ores]Setting glow level of "..ore.name.." to "..coal_glow_level)
-    description = ore.description
-    tiles = ore.tiles
-    groups = ore.groups
-    drop = ore.drop
-    is_ground_content = ore.is_ground_content
-    local paramtype = ore.paramtype
-    local use_texture_alpha = ore.use_texture_alpha
-    local drawtype = ore.drawtype
-    local sunlight_propagates = ore.sunlight_propagates
-    minetest.register_node(":default:stone_with_coal", {
-        description = description,
-        tiles = tiles,
-        groups = groups,
-        drop = drop,
-        is_ground_content = is_ground_content,
-        light_source = coal_glow_level,
-        paramtype = paramtype,
-        use_texture_alpha = use_texture_alpha,
-        drawtype = drawtype,
-        sunlight_propagates = sunlight_propagates,
-    })
-end    
-
-local is_glow_iron = minetest.settings:get_bool("is_glow_iron")
-local iron_glow_level = tonumber(minetest.settings:get("iron_glow_level"))
-ore = minetest.registered_nodes["default:stone_with_iron"]
-if ore and is_glow_iron then
-    --Redefine iron ore
-    minetest.log("[glow_ores]Setting glow level of "..ore.name.." to "..iron_glow_level)
-    description = ore.description
-    tiles = ore.tiles
-    groups = ore.groups
-    drop = ore.drop
-    is_ground_content = ore.is_ground_content
-    local paramtype = ore.paramtype
-    local use_texture_alpha = ore.use_texture_alpha
-    local drawtype = ore.drawtype
-    local sunlight_propagates = ore.sunlight_propagates
-    minetest.register_node(":default:stone_with_iron", {
-        description = description,
-        tiles = tiles,
-        groups = groups,
-        drop = drop,
-        is_ground_content = is_ground_content,
-        light_source = iron_glow_level,
-        paramtype = paramtype,
-        use_texture_alpha = use_texture_alpha,
-        drawtype = drawtype,
-        sunlight_propagates = sunlight_propagates,
-    })
-end    
-
-local is_glow_copper = minetest.settings:get_bool("is_glow_copper")
-local copper_glow_level = tonumber(minetest.settings:get("copper_glow_level"))
-ore = minetest.registered_nodes["default:stone_with_copper"]
-if ore and is_glow_copper then
-    --Redefine copper ore
-    minetest.log("[glow_ores]Setting glow level of "..ore.name.." to "..copper_glow_level)
-    description = ore.description
-    tiles = ore.tiles
-    groups = ore.groups
-    drop = ore.drop
-    is_ground_content = ore.is_ground_content
-    local paramtype = ore.paramtype
-    local use_texture_alpha = ore.use_texture_alpha
-    local drawtype = ore.drawtype
-    local sunlight_propagates = ore.sunlight_propagates
-    minetest.register_node(":default:stone_with_copper", {
-        description = description,
-        tiles = tiles,
-        groups = groups,
-        drop = drop,
-        is_ground_content = is_ground_content,
-        light_source = copper_glow_level,
-        paramtype = paramtype,
-        use_texture_alpha = use_texture_alpha,
-        drawtype = drawtype,
-        sunlight_propagates = sunlight_propagates,
-    })
-end    
-
-local is_glow_tin = minetest.settings:get_bool("is_glow_tin")
-local tin_glow_level = tonumber(minetest.settings:get("tin_glow_level"))
-ore = minetest.registered_nodes["default:stone_with_tin"]
-if ore and is_glow_tin then
-    --Redefine tin ore
-    minetest.log("[glow_ores]Setting glow level of "..ore.name.." to "..tin_glow_level)
-    description = ore.description
-    tiles = ore.tiles
-    groups = ore.groups
-    drop = ore.drop
-    is_ground_content = ore.is_ground_content
-    local paramtype = ore.paramtype
-    local use_texture_alpha = ore.use_texture_alpha
-    local drawtype = ore.drawtype
-    local sunlight_propagates = ore.sunlight_propagates
-    minetest.register_node(":default:stone_with_tin", {
-        description = description,
-        tiles = tiles,
-        groups = groups,
-        drop = drop,
-        is_ground_content = is_ground_content,
-        light_source = tin_glow_level,
-        paramtype = paramtype,
-        use_texture_alpha = use_texture_alpha,
-        drawtype = drawtype,
-        sunlight_propagates = sunlight_propagates,
-    })
-end    
-
-local is_glow_gold = minetest.settings:get_bool("is_glow_gold")
-local gold_glow_level = tonumber(minetest.settings:get("gold_glow_level"))
-ore = minetest.registered_nodes["default:stone_with_gold"]
-if ore and is_glow_gold then
-    --Redefine gold ore
-    minetest.log("[glow_ores]Setting glow level of "..ore.name.." to "..gold_glow_level)
-    description = ore.description
-    tiles = ore.tiles
-    groups = ore.groups
-    drop = ore.drop
-    is_ground_content = ore.is_ground_content
-    local paramtype = ore.paramtype
-    local use_texture_alpha = ore.use_texture_alpha
-    local drawtype = ore.drawtype
-    local sunlight_propagates = ore.sunlight_propagates
-    minetest.register_node(":default:stone_with_gold", {
-        description = description,
-        tiles = tiles,
-        groups = groups,
-        drop = drop,
-        is_ground_content = is_ground_content,
-        light_source = gold_glow_level,
-        paramtype = paramtype,
-        use_texture_alpha = use_texture_alpha,
-        drawtype = drawtype,
-        sunlight_propagates = sunlight_propagates,
-    })
-end    
-
-local is_glow_diamond = minetest.settings:get_bool("is_glow_diamond")
-local diamond_glow_level = tonumber(minetest.settings:get("diamond_glow_level"))
-ore = minetest.registered_nodes["default:stone_with_diamond"]
-if ore and is_glow_diamond then
-    --Redefine diamond ore
-    minetest.log("[glow_ores]Setting glow level of "..ore.name.." to "..diamond_glow_level)
-    description = ore.description
-    tiles = ore.tiles
-    groups = ore.groups
-    drop = ore.drop
-    is_ground_content = ore.is_ground_content
-    local paramtype = ore.paramtype
-    local use_texture_alpha = ore.use_texture_alpha
-    local drawtype = ore.drawtype
-    local sunlight_propagates = ore.sunlight_propagates
-    minetest.register_node(":default:stone_with_diamond", {
-        description = description,
-        tiles = tiles,
-        groups = groups,
-        drop = drop,
-        is_ground_content = is_ground_content,
-        light_source = diamond_glow_level,
-        paramtype = paramtype,
-        use_texture_alpha = use_texture_alpha,
-        drawtype = drawtype,
-        sunlight_propagates = sunlight_propagates,
-    })
-end    
-
-local is_glow_silver = minetest.settings:get_bool("is_glow_silver")
-local silver_glow_level = tonumber(minetest.settings:get("silver_glow_level"))
-ore = minetest.registered_nodes["moreores:mineral_silver"]
-if ore and is_glow_silver then
-    --Redefine silver ore
-    minetest.log("[glow_ores]Setting glow level of "..ore.name.." to "..silver_glow_level)
-    description = ore.description
-    tiles = ore.tiles
-    groups = ore.groups
-    drop = ore.drop
-    is_ground_content = ore.is_ground_content
-    local paramtype = ore.paramtype
-    local use_texture_alpha = ore.use_texture_alpha
-    local drawtype = ore.drawtype
-    local sunlight_propagates = ore.sunlight_propagates
-    minetest.register_node(":moreores:mineral_silver", {
-        description = description,
-        tiles = tiles,
-        groups = groups,
-        drop = drop,
-        is_ground_content = is_ground_content,
-        light_source = silver_glow_level,
-        paramtype = paramtype,
-        use_texture_alpha = use_texture_alpha,
-        drawtype = drawtype,
-        sunlight_propagates = sunlight_propagates,
-    })
-end    
-
-local is_glow_mithril = minetest.settings:get_bool("is_glow_mithril")
-local mithril_glow_level = tonumber(minetest.settings:get("mithril_glow_level"))
-ore = minetest.registered_nodes["moreores:mineral_mithril"]
-if ore and is_glow_mithril then
-    -- Redefine mineral_mithril node
-    minetest.log("[glow_ores]Setting glow level of "..ore.name.." to "..mithril_glow_level)
-    local description = ore.description
-    local tiles = ore.tiles
-    local groups = ore.groups
-    local drop = ore.drop
-    local is_ground_content = ore.is_ground_content
-    minetest.register_node(":moreores:mineral_mithril", {
-        description = description,
-        tiles = tiles,
-        groups = groups,
-        drop = drop,
-        is_ground_content = is_ground_content,
-        light_source = mithril_glow_level,
-        paramtype = "light",
-        use_texture_alpha = true,
-        drawtype = "glasslike",
-        sunlight_propagates = true,
-    })
-end
-
-local is_glow_mese = minetest.settings:get_bool("is_glow_mese")
-local mese_glow_level = tonumber(minetest.settings:get("mese_glow_level"))
-ore = minetest.registered_nodes["default:stone_with_mese"]
-if ore and is_glow_mese then
-    -- Redefine stone_with_mese node
-    minetest.log("[glow_ores]Setting glow level of "..ore.name.." to "..mese_glow_level)
-    description = ore.description
-    tiles = ore.tiles
-    groups = ore.groups
-    drop = ore.drop
-    is_ground_content = ore.is_ground_content
-    minetest.register_node(":default:stone_with_mese", {
-        description = description,
-        tiles = tiles,
-        groups = groups,
-        drop = drop,
-        is_ground_content = is_ground_content,
-        light_source = mese_glow_level,
-        paramtype = "light",
-        use_texture_alpha = true,
-        drawtype = "glasslike",
-        sunlight_propagates = true,
-    })
-end
-
-local is_glow_rainbow = minetest.settings:get_bool("is_glow_rainbow")
-local rainbow_glow_level = tonumber(minetest.settings:get("rainbow_glow_level"))
-ore = minetest.registered_nodes["rainbow_ore:rainbow_ore_block"]
-if ore and is_glow_rainbow then
-    --Redefine Rainbow ore
-    minetest.log("[glow_ores]Setting glow level of "..ore.name.." to "..rainbow_glow_level)
-    description = ore.description
-    tiles = ore.tiles
-    groups = ore.groups
-    drop = ore.drop
-    is_ground_content = ore.is_ground_content
-    local paramtype = ore.paramtype
-    local use_texture_alpha = ore.use_texture_alpha
-    local drawtype = ore.drawtype
-    local sunlight_propagates = ore.sunlight_propagates
-    minetest.register_node(":rainbow_ore:rainbow_ore_block", {
-        description = description,
-        tiles = tiles,
-        groups = groups,
-        drop = drop,
-        is_ground_content = is_ground_content,
-        light_source = rainbow_glow_level,
-        paramtype = paramtype,
-        use_texture_alpha = use_texture_alpha,
-        drawtype = drawtype,
-        sunlight_propagates = sunlight_propagates,
-    })
-end    
-
+end)
